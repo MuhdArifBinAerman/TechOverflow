1a579c0312e86e2dc866380c07d348a40c7a600b
MUHAMMAD ARIF BIN AERMAN 2102135G <2102135G@student.tp.edu.sg>
2022-12-18 19:12:18 +0800

    changes made

---
+++ TechOverflow/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TechOverflow.Server.Data;
+using TechOverflow.Server.Models;
+
+[assembly: HostingStartup(typeof(TechOverflow.Server.Areas.Identity.IdentityHostingStartup))]
+namespace TechOverflow.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
